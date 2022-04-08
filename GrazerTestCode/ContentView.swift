ccff66320d53e2b86a74cf0afe3222bf8eb5eba6
Atli Sævar <atli@ankeri.is>
2022-04-08 23:20:42 +0000

    Initial Commit

---
+++ GrazerTestCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GrazerTestCode
+//
+//  Created by Atli Saevar on 8.4.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GrazerTestCode/GrazerTestCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrazerTestCodeApp.swift
+//  GrazerTestCode
+//
+//  Created by Atli Saevar on 8.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct GrazerTestCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
